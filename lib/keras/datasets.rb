39ba50d93fc6f4c679453ccf7deee2a9fa8a4859
xiantotikui <41904509+xiantotikui@users.noreply.github.com>
2018-08-10 09:12:31 +0200

    Update datasets.rb

--- lib/keras/datasets.rb
+++ lib/keras/datasets.rb
@@ -1,9 +1,15 @@
 module Datasets
   pyimport :keras
   extend self
+  
+  attr_reader :x_train, :y_train, :x_test, :y_test
 
   def cifar10
     ret = -> { keras.datasets.cifar10.load_data }
-    ret.()
+    arr = ret.()
+    @x_train = arr[0]
+    @y_train = arr[1]
+    @x_test = arr[2]
+    @y_test = arr[3]
   end
 end
