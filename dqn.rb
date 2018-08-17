10ec2b6027775cbd0edf3d2d6d144e2e12ef0e0e
Xiantotikui <mr.lukasz.fuszara@gmail.com>
2018-08-17 13:52:55 +0200

    0.0.9

--- dqn.rb
+++ dqn.rb
@@ -1,123 +1,164 @@
 require 'pycall/import'
 include PyCall::Import
 
-pyimport :gym
-
 require './lib/keras'
 
 python_version('~/miniconda3/bin/python')
 
 keras_import(['Model', 'Convolutional', 'Core', 'Optimizers'])
 
-class DQNAgent
-  attr_accessor :epsilon
-  attr_reader :memory
+class Environment
+  attr_reader :state, :action
 
-  def initialize(state_size, action_size)
-    @state_size = state_size
-    @action_size = action_size
-    @memory = []
-    @gamma = 0.95
-    @tau = 0.5
+  def initialize
+    pyimport :gym
+    @env = gym.make 'CartPole-v0'
+    states = @env.observation_space.shape[0]
+    actions = @env.action_space.n
+    @agent = Agent.new(states, actions)
+  end
+
+  def run
+    @steps = 0
+    @agent.epsilon = 1.0
+    state = @env.reset
+    done = false
+    while !done
+      @env.render
+      action = @agent.act(state)
+      array = @env.step(action)
+      next_state = array[0]
+      reward = array[1]
+      done = array[2]
+
+      @agent.observe [state, action, reward, next_state]
+      @agent.replay
+
+      state = next_state
+      @steps += 1
+    end
+
+    puts 'Total reward: ' + @steps.to_s
+  end
+end
+
+class Agent
+  attr_writer :epsilon
+  attr_reader :state
+
+  def initialize(states, actions)
+    @states = states
+    @actions = actions
+    @batch_size = 64
+
     @epsilon = 1.0
-    @epsilon_min = 0.01
+    @min_epsilon = 0.01
     @epsilon_decay = 0.995
-    @learning_rate = 0.001
+
+    @gamma = 0.95
+
+    @brain = Brain.new(@states, @actions, @batch_size)
+  end
+
+  def act(state)
+    if Random.rand(0.0..1.0) <= @epsilon
+      @state = Random.rand(0...@actions)
+    else
+      @state = Numpy.argmax @brain.predict state
+    end
+  end
+
+  def observe(sample)
+    @brain.write_memory sample
+    @epsilon *= @epsilon_decay
+  end
+
+  def replay
+    batch = @brain.read_memory
+    zero_state = Numpy.zeros(@states)
+
+    batch.size.times do |t|
+      state = batch[t][0]
+      action = batch[t][1]
+      reward = batch[t][2]
+      if batch[t][3].nil?
+        next_state = zero_state
+      else
+        next_state = batch[t][3]
+      end
+      predicted_state = @brain.predict(state)
+      predicted_next_state = @brain.predict(next_state)
+
+      target = predicted_state
+
+      if next_state.nil?
+        target[action] = reward
+      else
+        target[action] = reward + @gamma * Numpy.amax(predicted_next_state)
+      end
+
+      @brain.train(Numpy.reshape(state, [1, @states]), Numpy.reshape(target, [1, @actions]))
+    end
+  end
+end
+
+class Brain
+  def initialize(states, actions, batch_size)
+    @batch_size = batch_size
+
+    @states = states
+    @actions = actions
+
+    @memory = Memory.new(200)
     @model = build_model
   end
 
   def build_model
     model = Keras::Model.sequential
-    model.add(Keras::Core.dense(64, input_dim: @state_size, activation: 'relu'))
-    model.add(Keras::Core.dense(@action_size, activation: 'linear'))
-    model.compile(loss: 'mse', optimizer: Keras::Optimizers.adam(lr: @learning_rate))
+    model.add Keras::Core.dense 64, activation: 'relu', input_dim: @states
+    model.add Keras::Core.dense @actions, activation: 'linear'
+    model.compile loss: 'mse', optimizer: Keras::Optimizers.adam(lr: 0.0005, clipvalue: 1.0)
     model
   end
 
-  def remember(state, action, reward, next_state, done, last_reward)
-    @memory << [state, action, reward, next_state, done, last_reward]
+  def predict(state)
+    (@model.predict Numpy.reshape(state, [1, @states])).flatten
   end
 
-  def act(state)
-    if Random.rand(0.0..1.0) <= @epsilon
-      ret = Random.rand(0...@action_size)
-    else
-      ret = Numpy.argmax(@model.predict(state))
-    end
-    ret
+  def train(x, y)
+    @model.fit x, y, batch_size: @batch_size, epochs: 1, verbose: 0
   end
 
-  def sigmoid(x)
-    1.0 / (1.0 + Math.exp(-x))
+  def read_memory
+    @memory.sampl @batch_size
   end
 
-  def replay(batch_size)
-    minibatch = @memory.last(batch_size)
-    i = 0
-    while i < minibatch.size
-      target = @memory[i][2]
-      tmp = (@memory[i][2] + @gamma * Numpy.amax(@model.predict(@memory[i][3]))) + @tau * @memory[i][5]
-      if !@memory[i][4]
-        target = sigmoid(tmp.to_f)
-      else
-        target = -1.0 * sigmoid(tmp.to_f)
-      end
-      target_f = @model.predict(@memory[i][0])
-      if @memory[i][1].zero?
-        target_f[0][0] = target
-        target_f[0][1] = 0.0
-      else
-        target_f[0][0] = 0.0
-        target_f[0][1] = target
-      end
-      @model.fit(@memory[i][0], target_f, epochs: 1, verbose: 0)
-      if @epsilon > @epsilon_min
-        @epsilon *= @epsilon_decay
-      end
-      i += 1
-    end
-  end
-
-  def load(name)
-    @model.load_weights(name)
-  end
-
-  def save(name)
-    @model.load_weights(name)
+  def write_memory(sample)
+    @memory.add sample
   end
 end
 
-episodes = 1000
-
-env = gym.make('CartPole-v1')
-state_size = env.observation_space.shape[0]
-action_size = env.action_space.n
-agent = DQNAgent.new(state_size, action_size)
-done = false
-batch_size = 32
-
-episodes.times do |e|
-  state = env.reset()
-  agent.epsilon = 1.0
-  last_reward = []
-  state = Numpy.reshape(state, [1, state_size])
-  500.times do |t|
-    env.render()
-    action = agent.act(state)
-    tmp = env.step(action)
-    next_state = tmp[0]
-    reward = tmp[1]
-    done = tmp[2]
-    next_state = Numpy.reshape(next_state, [1, state_size])
-    last_reward = t
-    agent.remember(state, action, reward, next_state, done, last_reward)
-    state = next_state
-    if done
-      puts("episode: #{e}/#{episodes}, score: #{t}, e: #{agent.epsilon}")
-      break
-    end
-    if agent.memory.size > batch_size
-      agent.replay(batch_size)
-    end
+class Memory
+  def initialize(capacity)
+    @capacity = capacity
+    @memory = []
   end
+
+  def add(array)
+    @memory << array
+    @memory.shift if @memory.size > @capacity
+  end
+
+  def sampl(n)
+    @memory.sample(n)
+  end
+end
+
+environment = Environment.new
+
+i = 0
+while i < 1000
+  puts 'Step: ' + i.to_s
+  environment.run
+  i += 1
 end
